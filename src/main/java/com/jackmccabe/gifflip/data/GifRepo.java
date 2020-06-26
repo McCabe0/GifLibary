02c2b21e81bdea981fc001df917ab6bcc933581c
Jack M <jackmccabe1995@hotmail.co.uk>
2020-06-26 18:57:06 +0100

    yoda has arrived

--- src/main/java/com/jackmccabe/gifflip/data/GifRepo.java
+++ src/main/java/com/jackmccabe/gifflip/data/GifRepo.java
@@ -19,7 +19,11 @@ public class GifRepo {
             new Gif("book-dominos", LocalDate.of(2015,9,15), "BigUp", false,1),
             new Gif("compiler-bot", LocalDate.of(2015,2,13), "Kcurtis", true,3),
             new Gif("cowboy-coder", LocalDate.of(2015,2,13), "Grace Hopper", false,2),
-            new Gif("infinite-andrew", LocalDate.of(2015,8,23), "Marissa Mayer", true,1)
+            new Gif("infinite-andrew", LocalDate.of(2015,8,23), "Marissa Mayer", true,1),
+            new Gif("baby-yoda", LocalDate.of(2020,3,23), "Jack", true,3),
+            new Gif("yoda", LocalDate.of(2020,2,23), "Yoda", true,3),
+            new Gif("yoda-eating", LocalDate.of(2020,4,23), "Baby Yoda", true,3)
+
     );
 
     public Gif findByName(String name){
